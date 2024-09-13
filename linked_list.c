33fd70ac8be53d4633b7e826592c9129195fd76d
Sebastian Einarsson <sebastian.einarsson03@gmail.com>
2024-09-13 11:33:31 +0200

    added DOCSTRINGS

--- linked_list.c
+++ linked_list.c
@@ -1,10 +1,21 @@
 #include "linked_list.h"
 
+/**
+ * Initializes the linked list.
+ * 
+ * @param head A pointer to the head of the list.
+ */
 void list_init(Node **head) {
     mem_init(MEMORY_SIZE);
     *head = NULL;
 }
 
+/**
+ * Inserts a new node at the beginning of the list.
+ * 
+ * @param head A pointer to the head of the list.
+ * @param data The data to be inserted.
+ */
 void list_insert(Node **head, int data) {
     Node *newNode = (Node *)mem_alloc(sizeof(Node));
     if (newNode == NULL) {
@@ -16,6 +27,12 @@ void list_insert(Node **head, int data) {
     *head = newNode;
 }
 
+/**
+ * Inserts a new node after a given node.
+ * 
+ * @param prevNode A pointer to the node after which the new node will be inserted.
+ * @param data The data to be inserted.
+ */
 void list_insert_after(Node *prevNode, int data) {
     if (prevNode == NULL) {
         printf_red("Previous node cannot be NULL\n");
@@ -31,6 +48,13 @@ void list_insert_after(Node *prevNode, int data) {
     prevNode->next = newNode;
 }
 
+/**
+ * Inserts a new node before a given node.
+ * 
+ * @param head A pointer to the head of the list.
+ * @param nextNode A pointer to the node before which the new node will be inserted.
+ * @param data The data to be inserted.
+ */
 void list_insert_before(Node **head, Node *nextNode, int data) {
     if (nextNode == NULL) {
         printf_red("Previous node cannot be NULL\n");
@@ -59,6 +83,12 @@ void list_insert_before(Node **head, Node *nextNode, int data) {
     }
 }
 
+/**
+ * Deletes a node with the given data from the list.
+ * 
+ * @param head A pointer to the head of the list.
+ * @param data The data of the node to be deleted.
+ */
 void list_delete(Node **head, int data) {
     Node *temp = *head;
     Node *prev = NULL;
@@ -80,6 +110,13 @@ void list_delete(Node **head, int data) {
     mem_free(temp);
 }
 
+/**
+ * Searches for a node with the given data in the list.
+ * 
+ * @param head A pointer to the head of the list.
+ * @param data The data to search for.
+ * @return A pointer to the node with the given data, or NULL if not found.
+ */
 Node *list_search(Node **head, int data) {
     Node *current = *head;
     while (current != NULL) {
@@ -91,6 +128,11 @@ Node *list_search(Node **head, int data) {
     return NULL;
 }
 
+/**
+ * Displays the entire list.
+ * 
+ * @param head A pointer to the head of the list.
+ */
 void list_display(Node **head) {
     Node *current = *head;
     if (current == NULL) {
@@ -109,6 +151,13 @@ void list_display(Node **head) {
     printf("]\n");
 }
 
+/**
+ * Displays a selected range of the list.
+ * 
+ * @param head A pointer to the head of the list.
+ * @param startNode A pointer to the starting node of the range.
+ * @param endNode A pointer to the ending node of the range.
+ */
 void list_display_select(Node **head, Node *startNode, Node *endNode) {
     Node *current = startNode;
     if (current == NULL) {
@@ -120,13 +169,19 @@ void list_display_select(Node **head, Node *startNode, Node *endNode) {
     while (current != NULL && current != endNode->next) {
         printf("%d", current->data);
         current = current->next;
-        if (current) {
+        if (current != endNode->next) {
             printf(", ");
         }
     }
     printf("]\n");
 }
 
+/**
+ * Counts the number of nodes in the list.
+ * 
+ * @param head A pointer to the head of the list.
+ * @return The number of nodes in the list.
+ */
 int list_count_nodes(Node **head) {
     int count = 0;
     Node* current = *head;
@@ -137,6 +192,11 @@ int list_count_nodes(Node **head) {
     return count;
 }
 
+/**
+ * Cleans up the list by freeing all nodes.
+ * 
+ * @param head A pointer to the head of the list.
+ */
 void list_cleanup(Node **head) {
     Node* current = *head;
     Node* next;
--- test_linked_list.c
+++ test_linked_list.c
@@ -94,6 +94,110 @@ void test_search()
     list_cleanup(&head);
 }
 
+
+// EXTRA
+// Test list initialization
+void test_list_init() {
+    Node *head = NULL;
+    list_init(&head);
+    if (head != NULL) {
+        printf_red("FAIL [test_list_init]: Head should be NULL after initialization\n");
+    } else {
+        printf_green("PASS [test_list_init]: Head is NULL after initialization\n");
+    }
+}
+
+// Test inserting before a node
+void test_insert_before() {
+    Node *head = NULL;
+    list_init(&head);
+    list_insert(&head, 20);
+    list_insert(&head, 10);
+    Node *node20 = list_search(&head, 20);
+    list_insert_before(&head, node20, 15);
+    Node *found = list_search(&head, 15);
+    assert_node_value(found, 15, "test_insert_before");
+    list_cleanup(&head);
+}
+
+// Test displaying the list
+void test_display() {
+    Node *head = NULL;
+    list_init(&head);
+    list_insert(&head, 10);
+    list_insert(&head, 20);
+    printf("Expected list: [20, 10]\n");
+    printf("Actual list: ");
+    list_display(&head);
+    list_cleanup(&head);
+}
+
+// Test displaying a selected range of the list
+void test_display_select() {
+    Node *head = NULL;
+    list_init(&head);
+    list_insert(&head, 10);
+    list_insert(&head, 20);
+    list_insert(&head, 30);
+    Node *startNode = list_search(&head, 20);
+    Node *endNode = list_search(&head, 10);
+    printf("Expected selected list: [20, 10]\n");
+    printf("Actual selected list: ");
+    list_display_select(&head, startNode, endNode);
+    list_cleanup(&head);
+}
+
+// Test counting nodes
+void test_count_nodes() {
+    Node *head = NULL;
+    list_init(&head);
+    list_insert(&head, 10);
+    list_insert(&head, 20);
+    int count = list_count_nodes(&head);
+    if (count != 2) {
+        printf_red("FAIL [test_count_nodes]: Expected 2 but got %d\n", count);
+    } else {
+        printf_green("PASS [test_count_nodes]: Expected 2\n");
+    }
+    list_cleanup(&head);
+}
+
+// Test cleaning up the list
+void test_cleanup() {
+    Node *head = NULL;
+    list_init(&head);
+    list_insert(&head, 10);
+    list_insert(&head, 20);
+    list_cleanup(&head);
+    if (head != NULL) {
+        printf_red("FAIL [test_cleanup]: Head should be NULL after cleanup\n");
+    } else {
+        printf_green("PASS [test_cleanup]: Head is NULL after cleanup\n");
+    }
+}
+
+// Test inserting before and after
+void test_insert_all() {
+    printf_yellow("Starting test [insert_both]\n");
+    Node *head = NULL;
+    list_init(&head);
+    list_insert(&head, 30);
+    list_insert(&head, 50);
+    list_insert_before(&head, head->next, 70);
+    list_insert(&head, 20);
+    list_insert(&head, 10);
+    list_delete(&head, 50);
+    Node* node10 = list_search(&head, 10);
+    list_insert_after(node10, 50);
+    list_insert_before(&head, node10, 60);
+    list_insert(&head, 40);
+
+    printf("List should be: [40, 60, 10, 50, 20, 70, 30]\nYour was:       ");
+    list_display(&head);
+    printf("List should be: [60, 10, 50, 20, 70]\nYour was:       ");
+    list_display_select(&head, list_search(&head, 60), list_search(&head, 70));
+}
+
 // Main function to run all tests
 int main()
 {
@@ -102,5 +206,15 @@ int main()
     test_insert_after();
     test_delete();
     test_search();
+
+    // Extra
+    test_list_init();
+    test_insert_before();
+    test_display();
+    test_display_select();
+    test_count_nodes();
+    test_cleanup();
+    test_insert_all();
+    
     return 0;
 }
